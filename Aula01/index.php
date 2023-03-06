06ecfe6282dcfefca4f93defd853483b22a0c97f
Thiago802 <105284314+Thiago802@users.noreply.github.com>
2023-03-06 11:07:24 -0300

    Add files via upload

---
+++ Aula01/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    
+<h1> <?php
+echo "Ola Mundo"
+?> </h1>;
+<br>
+
+<h2> <?php
+ echo (5+5)
+?>
+<br>
+<h2> <?php
+ echo (5*5)
+?>
+<br>
+<h2> <?php
+echo (5-5)
+?>
+<br>
+<h2> <?php
+  echo (10/2)
+?>
+</body>
+</html>
